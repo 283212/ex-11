361b12e22df6e8204c52accee0390a6b2a76cbc6
Petr Kriz <283212@vutbr.cz>
2026-04-22 09:45:18 +0200

    bubble

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 
 def random_numbers(count, low=0, high=100):
@@ -35,6 +36,53 @@ def selection_sort(values):
 
 
 
+def bubble_sort(values):
+    plt.ion()
+    plt.show()
+
+
+    velikost = len(values)
+
+    for e in range(velikost):
+
+        p = 0
+
+        for j in range(velikost - 1):
+            prvni_index = p
+            druhy_index = p + 1
+
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+
+            if values[prvni_index] > values[druhy_index]:
+                values[prvni_index], values[druhy_index] = values[druhy_index], values[prvni_index]
+
+            p += 1
+
+        print(values)
+
+        velikost -= 1
+
+    plt.ioff()
+    plt.show()
+
+    return values
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
     print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
@@ -42,4 +90,5 @@ if __name__ == "__main__":
     small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
     print(small)
 
-    selection_sort(values)
+    #selection_sort(values)
+    bubble_sort(values)
