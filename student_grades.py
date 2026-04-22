caad74447915d187c1061a1726358f8f3b9e061a
Petr Kriz <283212@vutbr.cz>
2026-04-22 10:45:48 +0200

    main

--- student_grades.py
+++ student_grades.py
@@ -1,4 +1,4 @@
-import random_numbers() from sorting.py
+from sorting import random_numbers
 
 class StudentsGrades:
     def __init__(self, scores):
@@ -81,21 +81,23 @@ class StudentsGrades:
 
 
 if __name__ == "__main__":
-    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+    # results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+    #
+    # print(results.count())  # 9
+    # print(results.get_by_index(2))  # 91
+    # print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]
+    #
+    # print(results.get_grade(2))  # A (91 bodů)
+    # print(results.get_grade(6))  # A (100 bodů)
+    # print(results.get_grade(7))  # F (38 bodů)
+    #
+    # print(results.find(100))  # [6]
+    # print(results.find(50))  # [4]
+    # print(results.find(77))  # []
+    #
+    # print(results.get_sorted())  # [38, 42, 50, 58, 67, 73, 85, 91, 100]
+    # print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]  ← beze změny
 
-    print(results.count())  # 9
-    print(results.get_by_index(2))  # 91
-    print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]
-
-    print(results.get_grade(2))  # A (91 bodů)
-    print(results.get_grade(6))  # A (100 bodů)
-    print(results.get_grade(7))  # F (38 bodů)
-
-    print(results.find(100))  # [6]
-    print(results.find(50))  # [4]
-    print(results.find(77))  # []
-
-    print(results.get_sorted())  # [38, 42, 50, 58, 67, 73, 85, 91, 100]
-    print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]  ← beze změny
-
-
+    random_results = StudentsGrades(random_numbers(30, 0, 100))
+    print(random_results.count())
+    print(random_results.get_sorted())
